1f328eab2d94a07712bbcd9f708670754f2fa8a2
Rahul <mrrahul2016@gmail.com>
2022-02-16 16:03:49 +0530

    create dynamic pages

--- index.js
+++ index.js
@@ -2,7 +2,22 @@
 const http = require('http');
 
 const server = http.createServer( (req, res) => {
-    res.end('hello from server');
+    const pathName = req.url;
+if ( pathName == '/')
+{
+    res.end('Main Landing page '+ pathName);
+
+}
+if ( pathName == '/about') {
+    res.end('About page ');
+
+} else {
+    res.writeHead( 404 , {
+        'Content-type' :'text/html',
+        'my-own-header' : 'Hello-world'
+    });
+    res.end('<h1> Page not found </h1> ');
+}
 })
 
 server.listen(8000,'127.0.0.1', () => {
